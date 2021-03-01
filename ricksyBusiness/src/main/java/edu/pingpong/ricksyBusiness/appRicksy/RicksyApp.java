f2ca551c5ff5a5ce46c15520f560e967d6a53c0f
pau <pllinas@cifpfbmoll.eu>
2021-03-01 17:48:49 +0100

    docs: :memo: Build directories structure

---
+++ ricksyBusiness/src/main/java/edu/pingpong/ricksyBusiness/appRicksy/RicksyApp.java
@@ -0,0 +1,13 @@
+package edu.pingpong.ricksyBusiness.appRicksy;
+
+/**
+ * Hello world!
+ *
+ */
+public class RicksyApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
