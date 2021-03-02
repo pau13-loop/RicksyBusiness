875be2e135fb2c603e5e3ea998f26295286265d2
pau <pllinas@cifpfbmoll.eu>
2021-03-02 20:11:54 +0100

    feat: :sparkles: Test cases for pay new method

--- ricksyBusiness/src/test/java/edu/pingpong/ricksyBusiness/appRicksy/CreditCardTest.java
+++ ricksyBusiness/src/test/java/edu/pingpong/ricksyBusiness/appRicksy/CreditCardTest.java
@@ -1,18 +1,31 @@
 package edu.pingpong.ricksyBusiness.appRicksy;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class CreditCardTest {
 
-    private CreditCard creditCard = null;
+    private CreditCard creditCard;
     private double delta = 0.01;
 
     @Before
     public void setupCreditCard() {
         creditCard = new CreditCard("Montgomeri Burns", "666777999");
+    }
+
+    @Test
+    public void payTrueTest() {
+        assertTrue(creditCard.pay(1000));
+        assertEquals(2000, creditCard.credit(), delta);
+    }
+    
+    @Test
+    public void payFalseTest() {
+        assertFalse(creditCard.pay(5000));
     }
 
     @Test
