61da9be8e803f51e70766ae374fe8ba62b70803e
pau <pllinas@cifpfbmoll.eu>
2021-03-02 21:56:41 +0100

    test: :white_check_mark: Adding some more test for UosPark class

--- ricksyBusiness/src/test/java/edu/pingpong/ricksyBusiness/appRicksy/UfosParkTest.java
+++ ricksyBusiness/src/test/java/edu/pingpong/ricksyBusiness/appRicksy/UfosParkTest.java
@@ -2,6 +2,14 @@ package edu.pingpong.ricksyBusiness.appRicksy;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -24,7 +32,6 @@ public class UfosParkTest {
     @Test
     public void dispatchTest() {
         ufosPark.dispatch(creditCard);
-
         assertEquals(2500, creditCard.credit(), delta);
         assertEquals("Ovni2", ufosPark.getUfoOf("666777999"));
     }
@@ -38,5 +45,16 @@ public class UfosParkTest {
     public void toStringTest() {
         String expected = "[Ovni1, Ovni2, Ovni3]";
         assertEquals(expected, ufosPark.toString());
+    }
+
+    @Test
+    public void containsCardTest() {
+        ufosPark.dispatch(creditCard);
+        assertTrue(ufosPark.containsCard("666777999"));
+    }
+
+    @Test
+    public void notContainsCardTest() {
+        assertFalse(ufosPark.containsCard("666777999"));
     }
 }
