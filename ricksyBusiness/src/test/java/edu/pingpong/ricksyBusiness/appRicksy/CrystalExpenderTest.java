90062f2f3d4e91e3a131e48adb7698ae410c7ce8
pau <pllinas@cifpfbmoll.eu>
2021-03-02 20:57:56 +0100

    test: :white_check_mark: Creation test cases for CrystalExpender class

---
+++ ricksyBusiness/src/test/java/edu/pingpong/ricksyBusiness/appRicksy/CrystalExpenderTest.java
@@ -0,0 +1,31 @@
+package edu.pingpong.ricksyBusiness.appRicksy;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CrystalExpenderTest {
+
+    private CreditCard card;
+    private CrystalExpender crystal;
+    
+    @Before
+    public void setupCrystal() {
+        card = new CreditCard("Montgomeri Burns", "666777999");
+        crystal = new CrystalExpender(10, 500.0);
+    }
+
+    @Test
+    public void stockTest() {
+        crystal.dispatch(card);
+        crystal.dispatch(card);
+        assertEquals(8, crystal.stock());
+    }
+
+    @Test
+    public void toStringTest() {
+        String expected = "Stock: 10\nCost: 500.0";
+        assertEquals(expected, crystal.toString());
+    }
+}
